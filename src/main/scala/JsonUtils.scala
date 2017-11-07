8aa7cb1aa4f1c4f8ee8ecd75b26f650c1c3da9d3
Mikhail Prushinsky (DHL RU) <Mikhail.Prushinsky@dhl.com>
2017-11-07 16:26:10 +0300

    remove null values recursive implemented

--- src/main/scala/JsonUtils.scala
+++ src/main/scala/JsonUtils.scala
@@ -12,6 +12,39 @@ object JsonUtils {
   }
 
   /**
+    * Recursive implementation of removeNullValues function
+    * @param json Json object
+    * @return new Json object with null nodes removed
+    */
+  def removeNullValuesRecursive(json: Json): Json = {
+    json.hcursor.keys.get.foldLeft(json: Json)((json, key) => deleteIfNullValue(json, key))
+  }
+
+  /**
+    * Helper function which deletes node from Json if value is null and if value is other json object runs itself for each key of this object
+    * @param json Json object
+    * @param key Key to check
+    * @return new Json object with null nodes removed
+    */
+  def deleteIfNullValue(json: Json, key: String): Json = {
+    val cursor = json.hcursor.downField(key)
+    cursor.keys match {
+      case Some(keys) => {
+        keys.foldLeft(json: Json)((json, key) => cursor.withFocus(json => deleteIfNullValue(json, key)).top.get)
+      }
+      case None => {
+        if(cursor.focus.get.isNull){
+          cursor.delete.top.getOrElse(Json.Null)
+        }
+        else{
+          cursor.top.get
+        }
+      }
+    }
+  }
+
+
+  /**
     * Loads Json from string
     * @param jsonString - String to load
     * @return Json object
